f1e3b1970068f9e83c2b79d2662db5a9a8294ea6
Iasmyn Cristyne 2InfoB <iasmyncastro009@gmail.com>
2026-09-24 20:19:16 -0300

    Implement loop to input numbers until zero is entered

---
+++ problema10.py
@@ -0,0 +1,8 @@
+while True:
+    numero = int(input("Digite um número (0 para sair): "))
+
+    if numero == 0:
+        print("Programa encerrado.")
+        break
+
+    print("Você digitou:", numero)
